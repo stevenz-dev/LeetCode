b24b6bb88ec9384ddafdfd538a3878a75c4abe7a
Steven Zhang <stevenz@whitehardt.com>
2025-09-06 20:02:44 -0500

    - add Array - 125. Valid Palindrome

---
+++ Array/Array - 2 Pointers/125. Valid Palindrome.py
@@ -0,0 +1,43 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        l, r = 0, len(s)-1
+        while l < r:
+            if not s[l].isalnum():
+                l += 1
+            elif not s[r].isalnum():
+                r -= 1
+            else:
+                if s[l].lower() != s[r].lower():
+                    return False
+                else:
+                    l += 1
+                    r -= 1
+        return True
+
+    def isPalindrome(self, s: str) -> bool:
+        left, right = 0, len(s) - 1
+
+        while left < right:
+            # Skip non-alphanumeric characters
+            while left < right and not s[left].isalnum():
+                left += 1
+            while left < right and not s[right].isalnum():
+                right -= 1
+                
+            # Compare ignoring case
+            if s[left].lower() != s[right].lower():
+                return False
+
+            left += 1
+            right -= 1
+
+        return 
+
+
+solution = Solution()
+s = "A man, a plan, a canal: Panama"
+print(solution.isPalindrome(s))
