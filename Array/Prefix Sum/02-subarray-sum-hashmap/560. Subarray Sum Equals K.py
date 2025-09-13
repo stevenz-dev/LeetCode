e842d85a5478237d3946db82fee6788b5ca2253c
Steven Zhang <stevenzhanguk@gmail.com>
2025-09-13 10:49:24 -0500

    - add Array - 560. Subarray Sum Equals K

---
+++ Array/Prefix Sum/02-subarray-sum-hashmap/560. Subarray Sum Equals K.py
@@ -0,0 +1,24 @@
+from typing import List
+from collections import defaultdict
+
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        count = defaultdict(int)
+        count[0] = 1  # base case: 前缀和本身等于 k
+        res = 0
+        pre_sum = 0
+
+        for num in nums:
+            pre_sum += num
+            if (pre_sum - k) in count:
+                res += count[pre_sum - k]
+            count[pre_sum] += 1
+            print(pre_sum)
+        print(count)
+
+        return res
+
+
+nums = [1,1,1]
+k = 2
+print(Solution().subarraySum(nums, k))
